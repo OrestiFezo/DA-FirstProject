5725a9dcfb5d61aa811f6dc7417fa1f507870bb5
Elsa Tafani <elsatafani@icloud.com>
2025-07-31 20:59:09 +0200

    Contact Us page added.

--- main.js
+++ main.js
@@ -17,3 +17,33 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+//Contact Form Validation
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const message = document.getElementById('message').value.trim();
+
+    const emailRegex = /^[^@]+@[^@]+\.[a-z]{2,}$/i;
+    if (!emailRegex.test(email)) {
+      alert("Please enter a valid email address.");
+      return;
+    }
+    if (!name || !email || !message) {
+      // Show error
+      formMessage.className = "alert alert-danger";
+      formMessage.textContent = "Please fill in all fields.";
+      formMessage.classList.remove("d-none");
+    } 
+    else {
+      formMessage.className = "alert alert-success";
+      formMessage.textContent = "Your message has been sent successfully. Thank you!";
+      formMessage.classList.remove("d-none");
+      document.getElementById("contactForm").reset();
+    }
+    // Hide message after 5 seconds
+    setTimeout(() => {
+      formMessage.classList.add("d-none");
+    }, 5000);
+  });
+  
